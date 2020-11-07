4b9737440fe4526c0070825213cf1a39c1ed7911
Michael Notter <michaelnotter@hotmail.com>
2020-11-07 21:16:25 +0100

    Update to realistic case, even if it fails on circleci

--- workshop/test_notebooks.py
+++ workshop/test_notebooks.py
@@ -5,11 +5,10 @@ if __name__ == '__main__':
 
     # Test workshop notebooks
     notebooks = sorted(glob('/home/neuro/workshop/notebooks/*.ipynb'))
-    notebooks = [n for n in notebooks if '03a_functional_connectivity' not in n]
     notebooks = [n for n in notebooks if 'hidden_diffusion_imaging' not in n]
 
     # Test the notebooks
     for test in notebooks:
-        cmd = 'pytest --nbval-lax --nbval-cell-timeout 10000 -v -s %s' % test
+        cmd = 'pytest --nbval-lax --nbval-cell-timeout 7200 -v -s %s' % test
         print(cmd)
         os.system(cmd)
